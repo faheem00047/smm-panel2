c4a2098f891ee7c531cfba8d119b5fe5e9fdd25c
Ewriq <102306313+ewriq@users.noreply.github.com>
2023-03-25 18:02:40 +0300

    Add files via upload

---
+++ admin/controller/callback.php
@@ -0,0 +1,8 @@
+<?php
+  if( $user["access"]["callback"] != 1  ):
+    header("Location:".site_url("admin"));
+    exit();
+  endif;
+require admin_view('callback');
+
+?>
---
+++ admin/controller/twice.php
@@ -0,0 +1,8 @@
+<?php
+  if( $user["access"]["twice"] != 1  ):
+    header("Location:".site_url("admin"));
+    exit();
+  endif;
+require admin_view('twice');
+
+?>
